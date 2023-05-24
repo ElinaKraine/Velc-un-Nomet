8a392251c5a7dc002a014d5261f786521350450d
Kraine <elina.kraine@sk.lvt.lv>
2023-05-24 12:19:08 +0300

    Iesakta drag drop skripta rakstisana

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropSkripts : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    //Uzglabās norādi uz Objekti skriptu
+    public Object objektuSkripts;
+    //Uzglabās norādi uz katra objekta CanvasGroup
+    private CanvasGroup kanvasGrupa;
+    private RectTransform velkObjRectTransf;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    void Start () 
+    {
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        velkObjRectTransf = GetComponent<RectTransform>();
+    }
+	
+}
